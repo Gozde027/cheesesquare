edb729c5eda4725f65bddea2f84fcbb9d7e9facc
gozde.kaval@gmail.com <Fatistan.git123>
2018-05-31 23:28:54 +0300

    Finish detail activity when arrow pressed to keep previous state

--- app/src/main/java/com/support/android/designlibdemo/CheeseDetailActivity.java
+++ app/src/main/java/com/support/android/designlibdemo/CheeseDetailActivity.java
@@ -22,6 +22,7 @@ import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
@@ -64,4 +65,18 @@ public class CheeseDetailActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.sample_actions, menu);
         return true;
     }
+
+    //with this method, we understand that user pressed navigation arrow button
+    //If this activity finishes, previous one will remain on its current state
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
